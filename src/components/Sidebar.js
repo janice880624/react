72be76e4dbdf96efa636d63ebeeb9b3b935aedd2
janicekuo <47550853+janice880624@users.noreply.github.com>
2023-04-26 17:56:37 +0800

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,17 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Sidebar(){
     return (
         <div>
-            <h1> Sidebar </h1>
+            <p> There is Sidebar </p>
+            <ul>
+                <li><Link to="/"> Dashboard </Link></li>
+                <li><Link to="/Settings"> Setting </Link></li>
+                <li><Link to="/Visuallizations"> Visuallizations </Link></li>
+            </ul>
         </div>
     );
 }
 
 export default Sidebar;
-
-
