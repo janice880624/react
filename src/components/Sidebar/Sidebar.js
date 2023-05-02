54249fc95efb75a685cfa62c983ffd227c90b3c5
janicekuo <47550853+janice880624@users.noreply.github.com>
2023-05-02 19:56:11 +0800

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,185 +1,66 @@
 import React from "react";
-import styled from "styled-components";
-import headphoto from "../../img/head.jpg";
+import ListSubheader from '@mui/material/ListSubheader';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import SendIcon from '@mui/icons-material/Send';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import StarBorder from '@mui/icons-material/StarBorder';
 import "./Sidebar.css";
-import { BsPieChart } from "react-icons/bs";
-import { BiFolder, BiBookOpen, BiShoppingBag } from "react-icons/bi";
-import { TbBrandWechat, TbNotebook } from "react-icons/tb";
-import { FaPeopleArrows, FaUser } from "react-icons/fa";
-import { MdSwitchAccount} from "react-icons/md";
-
-const Account = styled.div`
-    text-align: center;
-    display: flex;
-    flow-wrap: nowrap;
-    align-items: center;
-    p{
-        margin-left: 10px;
-        font-size: 20px;
-    }
-`
-const Favorites = styled.div`
-    text-align: left;
-    justify-content: center;
-
-    div{
-        display: flex;
-        flow-wrap: nowrap;
-        margin: 6px 0px;
-        p {
-            margin: 4px;
-            margin-right: 10px;
-            font-size: 18px;
-        }
-        ul{
-            margin: 0;
-            padding: 0;
-            margin-left: 30px;
-            li{
-                margin: 4px;
-                font-size: 18px;
-            }
-        }
-    }
-`
-const Dashboards = styled.div`
-    text-align: left;
-    justify-content: center;
-    margin-top: 20px;
-    div{
-        div{
-            display: flex;
-            flow-wrap: nowrap;
-            align-items: center;
-            margin: 6px 0px;
-            margin-left: 10px;
-            p{
-                margin-left: 10px;
-            }
-        }
-        p {
-            margin: 4px;
-            margin-right: 10px;
-            font-size: 18px;
-        }
-        ul{
-            margin: 0;
-            padding: 0;
-            margin-left: 20px;
-            li{
-                margin: 4px;
-                font-size: 18px;
-            }
-        }
-    }
-`
-const Pages = styled.div`
-    text-align: left;
-    justify-content: center;
-    margin-top: 20px;
-    div{
-        div{
-            display: flex;
-            flow-wrap: nowrap;
-            align-items: center;
-            margin: 6px 0px;   
-            margin-left: 10px;
-        }
-        p {
-            margin: 4px;
-            margin-right: 10px;
-            font-size: 18px;
-        }
-        div{
-            p{
-                margin-left: 10px;
-            }
-            li{
-                list-style-type: none;
-                font-size: 18px;
-            }
-        }
-    }
-`
 
 function Sidebar(){
+    const [open, setOpen] = React.useState(true);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
     return (
-        <div>
-            <Account>
-                <img src={headphoto} alt="headphoto" className="headphoto"/>
-                <p> Username </p>
-            </Account>
-
-            <Favorites>
-                <div>
-                    <p style={{color: "rgba(0, 0, 0, 0.6)", fontWeight: "500"}}> Favorites </p>
-                    <p style={{color: "rgba(0, 0, 0, 0.4)", fontWeight: "500"}}> Recently </p>
-                </div>
-                <div>
-                    <ul>
-                        <li> Overview </li>
-                        <li> Project </li>
-                    </ul>
-                </div>
-            </Favorites>
-
-            <Dashboards>
-                <div>
-                    <p style={{color: "rgba(0, 0, 0, 0.4)", fontWeight: "500"}}> Dashboards </p>
-                    <div>
-                        <i className="arrow right"></i>
-                        <BsPieChart />
-                        <p> Default </p>
-                    </div>
-                    <div>
-                        <i className="arrow right"></i>
-                        <BiShoppingBag />
-                        <p> E-commerce </p>
-                    </div>
-                    <div>
-                        <i className="arrow right"></i>
-                        <BiFolder />
-                        <p> Projects </p>
-                    </div>
-                    <div>
-                        <i className="arrow right"></i>
-                        <BiBookOpen />
-                        <p> Online Courses </p>
-                    </div>
-                </div>
-            </Dashboards>
-
-            <Pages>
-                <div>
-                    <p style={{color: "rgba(0, 0, 0, 0.4)", fontWeight: "500"}}> Pages </p>
-                    <div>
-                        <i className="arrow right"></i>
-                        <FaUser />
-                        <p> User Profile </p>
-                    </div>
-                    <div>
-                        <i className="arrow right"></i>
-                        <MdSwitchAccount />
-                        <p> Account </p>
-                    </div>
-                    <div>
-                        <i className="arrow right"></i>
-                        <FaPeopleArrows />
-                        <p> Corporate </p>
-                    </div>
-                    <div>
-                        <i className="arrow right"></i>
-                        <TbNotebook />
-                        <p> Blog </p>
-                    </div>
-                    <div>
-                        <i className="arrow right"></i>
-                        <TbBrandWechat />
-                        <p> Social </p>
-                    </div>
-                </div>
-            </Pages>
-        </div>
+        <List
+        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+            Nested List Items
+            </ListSubheader>
+        }
+        >
+        <ListItemButton>
+            <ListItemIcon>
+            <SendIcon />
+            </ListItemIcon>
+            <ListItemText primary="Sent mail" />
+        </ListItemButton>
+        <ListItemButton>
+            <ListItemIcon>
+            <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+        </ListItemButton>
+        <ListItemButton onClick={handleClick}>
+            <ListItemIcon>
+            <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+            {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 4 }}>
+                <ListItemIcon>
+                <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary="Starred" />
+            </ListItemButton>
+            </List>
+        </Collapse>
+        </List>
     );
 }
 
